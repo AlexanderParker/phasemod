34fbdb359cf049b873c29558b0a5169f109da569
Alexander Parker <alexofparker@gmail.com>
2012-03-20 03:24:29 +0800

    Added and tested phase modulation code

--- js/oscillator.js
+++ js/oscillator.js
@@ -47,6 +47,7 @@ Oscillator.prototype.setAmplitude = function(amplitude) {
 	};
 }
 
+
 Oscillator.prototype.setFrequency = function(frequency) {
 	if (typeof(frequency) == 'number') {
 		this.frequency = frequency;
@@ -55,6 +56,9 @@ Oscillator.prototype.setFrequency = function(frequency) {
 	};
 }
 
+/**
+ * Assigns a second oscillator to modulate this oscillator
+ */
 Oscillator.prototype.setPhaseOscillator = function(phaseOscillator) {
 	if (typeof(phaseOscillator) == 'object') {
 		console.log();
@@ -62,8 +66,11 @@ Oscillator.prototype.setPhaseOscillator = function(phaseOscillator) {
 	}
 }
 
+/**
+ * Sets the amount by which the phase oscillator modulates this oscillator
+ */
 Oscillator.prototype.setPhaseOscillatorAmount = function(phaseOscillatorAmount) {
-	if (typeof(frequency) == 'number') {
+	if (typeof(phaseOscillatorAmount) == 'number') {
 		this.phaseOscillatorAmount = phaseOscillatorAmount;
 	} else {
 		throw 'setPhaseOscillatorAmount only accepts numeric values';	
@@ -73,10 +80,8 @@ Oscillator.prototype.setPhaseOscillatorAmount = function(phaseOscillatorAmount) 
 Oscillator.prototype.process = function(e) {
 	this.buffer = e.outputBuffer.getChannelData(0);
 	for (var i = 0; i < this.buffer.length; i++) {
-		this.buffer[i] = this.getSample();;
-		/** @todo make this patchable **/
-		//var phaseMod = this.phaseOffset * Math.PI * 2.0;
-		this.phase += this.frequency / this.sampleRate ;//+ phaseMod;
+		this.buffer[i] = this.getSample();
+		this.phase += this.frequency / this.sampleRate + this.calculatePhaseModulation(i);
 		while (this.phase > 1.0) this.phase -= 1;
 	}
 }
@@ -102,11 +107,28 @@ Oscillator.prototype.pause = function() {
  * current phase offset
  */
 Oscillator.prototype.getSample = function() {
-return this.amplitude * Math.sin( this.phase * Math.PI * 2.0 );
 	switch (this.shape) {
 		case 'sine':
 		default: 
 			return this.amplitude * Math.sin( this.phase * Math.PI * 2.0 );
 		break;
+	}
+}
+
+/**
+ * Calculates the phase modulation offset
+ */ 
+Oscillator.prototype.calculatePhaseModulation = function( offset ) {
+	if (
+		typeof(this.phaseOscillator == 'object')
+		&& this.phaseOscillator.playing == true		
+		&& this.phaseOscillatorAmplitude > 0
+		&& this.phaseOscillator.buffer != null
+		&& this.phaseOscillator.buffer.length == this.buffer.length
+	) {	
+		//Phase lock will suffice for now...
+		return this.phaseOscillator.buffer[offset] * this.phaseOscillatorAmplitude;
+	} else {
+		return 0;
 	}
 }
